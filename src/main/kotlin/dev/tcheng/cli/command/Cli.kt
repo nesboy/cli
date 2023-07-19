007fdbbfe506e62328e195a8961e6a183e882fc4
Tyrone Cheng <nesboy99@gmail.com>
2023-07-19 16:03:50 -0700

    Fix HelpFormatter

--- src/main/kotlin/dev/tcheng/cli/command/Cli.kt
+++ src/main/kotlin/dev/tcheng/cli/command/Cli.kt
@@ -2,11 +2,18 @@ package dev.tcheng.cli.command
 
 import com.github.ajalt.clikt.core.NoOpCliktCommand
 import com.github.ajalt.clikt.core.context
+import com.github.ajalt.clikt.output.MordantHelpFormatter
 
 object Cli : NoOpCliktCommand() {
     init {
         context {
-            // figure out HelpFormatter
+            helpFormatter = {
+                MordantHelpFormatter(
+                    context = it,
+                    showDefaultValues = true,
+                    showRequiredTag = true
+                )
+            }
         }
     }
 }
