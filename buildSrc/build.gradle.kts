088e7a36f42490d0a3d3bb32b205581000e15940
Tyrone Cheng <nesboy99@gmail.com>
2023-07-19 15:49:14 -0700

    Upgrade dependencies
    * Kotlin 1.9.0
    * Gradle 8.2.1
    * Detekt 1.23.0
    * Clikt 4.1.0
    * Jacksn 2.15.2

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,7 +7,7 @@ repositories {
 }
 
 dependencies {
-    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.22.0")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.21")
+    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.23.0")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
     implementation("org.jetbrains.kotlinx:kover:0.6.1")
 }
